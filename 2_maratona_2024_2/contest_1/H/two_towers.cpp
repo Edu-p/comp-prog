bb21072c1412d811df10c9d470fc7d3622918694
Edu-p <eduardoaamorim0@gmail.com>
2025-01-22 08:25:36 -0300

    fix: two_towers upsolving

--- 2_maratona_2024_2/contest_1/H/two_towers.cpp
+++ 2_maratona_2024_2/contest_1/H/two_towers.cpp
@@ -8,20 +8,19 @@ string is_beauty_towers(string f_tower, string s_tower){
     string full_string;
     full_string = f_tower + string(s_tower.rbegin(), s_tower.rend());
     
+    int counter = 0;
+
     for (int i=0; i<full_string.size(); ++i){
         // checa se tem 3 repetidas
         char char_da_vez = full_string[i];
-        if (full_string[i+1] == char_da_vez && full_string[i+2] == char_da_vez){
-            return "NO";
-        }  
-        // checa se tem 2 e 2(regex way better though)
-        if (full_string[i] == 'B' && full_string[i+1] == 'B' && full_string[i] == 'R' && full_string[i+1] == 'R'){
-            return "NO";
-        }
-        if (full_string[i] == 'R' && full_string[i+1] == 'R' && full_string[i] == 'B' && full_string[i+1] == 'B'){
-            return "NO";
-        }
+        if (full_string[i+1] == char_da_vez){
+            counter++;
+            if (counter>1){
+                return "NO";
+            }
+        } 
     }
+    
     // se nao passar em nenhum dos casos da para deixar beauty
     return "YES";
 }
