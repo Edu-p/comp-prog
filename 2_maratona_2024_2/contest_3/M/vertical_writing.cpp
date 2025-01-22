a80931e04bb1c346d1db844f2ccecdbb39d8e143
Edu-p <eduardoaamorim0@gmail.com>
2025-01-22 08:26:31 -0300

    fix: vertical_writing upsolving

--- 2_maratona_2024_2/contest_3/M/vertical_writing.cpp
+++ 2_maratona_2024_2/contest_3/M/vertical_writing.cpp
@@ -17,31 +17,28 @@ int main() {
     }
 
     // encontrar o tamanho da maior string 
-    int tam_max = INT_MIN;
+    int tam_max = 0;
 
-    for (auto str_da_vez : main_vec){
+    for (const auto &str_da_vez : main_vec){
         tam_max = max(static_cast<int>(str_da_vez.size()), tam_max);
     }
 
-    for (auto str_da_vez : main_vec){
+    for (auto &str_da_vez : main_vec){
         if (str_da_vez.size() < tam_max){
-            for (int i=(tam_max-str_da_vez.size()); i<tam_max; ++i){
-                str_da_vez.append("*");
-            }
+            str_da_vez.append(string(tam_max - str_da_vez.size(), '*'));
         }
     }
 
-    for (auto str_da_vez : main_vec){
-        cout << str_da_vez << '\n';
+    for(int j=0; j< tam_max; ++j){
+        string result_row;
+        for(int i=n - 1; i>=0; --i){
+            result_row += main_vec[i][j];
+        }
+        while (!result_row.empty() && result_row.back() == '*'){
+            result_row.pop_back();
+        }
+        cout << result_row << '\n';
     }
-
-    // // da ultima string para a primeira  
-    // for (int j=0; j<tam_max; ++j){
-    //     for (int i=(main_vec.size() - 1); i>=0; --i){
-    //         cout << main_vec[i][j];
-    //     }
-    //     cout << '\n';
-    // }
-    
+    return 0;
 
 }
