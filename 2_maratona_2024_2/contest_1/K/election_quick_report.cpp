cee5dbbc369f5c652daf7dc6dc387d8831469d0b
Edu-p <eduardoaamorim0@gmail.com>
2025-01-22 08:25:49 -0300

    fix: election_quick_report upsolving

--- 2_maratona_2024_2/contest_1/K/election_quick_report.cpp
+++ 2_maratona_2024_2/contest_1/K/election_quick_report.cpp
@@ -1,41 +1,44 @@
 #include <iostream>
 #include <vector>
 #include <climits>
+#include <tuple>
 
 using namespace std;
-
-void who_winner(vector<int>& ref_candi){
-    int qtd_votes_bigger = 0;
-    int index_votes_bigger = 0;
-
-    for (int i=0; i<ref_candi.size(); ++i) {
-        if (ref_candi[i]>qtd_votes_bigger){
-            qtd_votes_bigger = ref_candi[i];
-            index_votes_bigger = i;
-        }
-    }
-    
-    cout <<  index_votes_bigger+1 << endl;
-    
-}
 
 int main(){
     int n;
     cin >> n;
-    vector<int> candi(n);
+    vector<int> candi(n, 0);
 
     for (int i=0; i<n; ++i){
-        candi[i] == 0;
+        candi[i] = 0;
     }
     
     int m;
     cin >> m;
     vector<int> votes(m);
+    tuple<int, int> info_winner = {0, 0};
+
+    /*
+        (n_of_winner, how_many_votes_winner)
+    */ 
 
     for (int i=0; i<m; ++i){
         int vote;
         cin >> vote;
         candi[vote-1] += 1;
-        who_winner(candi);
+
+        if (i==0) {
+            get<0>(info_winner) = vote;
+            get<1>(info_winner) = candi[vote-1]; 
+        }
+        else {
+            // if candi[vote-1] > get<1>
+            if ((candi[vote-1] > get<1>(info_winner)) || (candi[vote-1] == get<1>(info_winner)  && vote < get<0>(info_winner))){
+                get<0>(info_winner) = vote;
+                get<1>(info_winner) = candi[vote-1];
+            }
+        }
+        cout << get<0>(info_winner) << "\n";
     }
 }
